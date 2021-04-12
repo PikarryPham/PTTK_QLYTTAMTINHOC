c800c919a0c70b54174c58fa5453eb65941d4d21
unknown <banhu322000@gmail.com>
2021-04-12 13:24:28 +0700

    UI-PhanCCongGiangDay

---
+++ GUI_PTTK/PhanCongGiangDay.Designer.cs
@@ -0,0 +1,292 @@
+﻿
+namespace GUI_PTTK
+{
+    partial class PhanCongGiangDay
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txTenHocPhan = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txGV = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txPhongHoc = new System.Windows.Forms.TextBox();
+            this.txTGBT = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.txTGKT = new System.Windows.Forms.TextBox();
+            this.Phong = new System.Windows.Forms.Label();
+            this.txthu = new System.Windows.Forms.TextBox();
+            this.btnPCGD = new System.Windows.Forms.Button();
+            this.dataGridView3 = new System.Windows.Forms.DataGridView();
+            this.label8 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.BackgroundColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(196, 79);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(372, 199);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(193, 40);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(139, 17);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Danh sach hoc phan";
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.BackgroundColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Location = new System.Drawing.Point(637, 79);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.RowHeadersWidth = 51;
+            this.dataGridView2.RowTemplate.Height = 24;
+            this.dataGridView2.Size = new System.Drawing.Size(372, 199);
+            this.dataGridView2.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(634, 40);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(137, 17);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Danh sach giao vien";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(193, 299);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(96, 17);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Ten hoc phan";
+            // 
+            // txTenHocPhan
+            // 
+            this.txTenHocPhan.Location = new System.Drawing.Point(336, 299);
+            this.txTenHocPhan.Name = "txTenHocPhan";
+            this.txTenHocPhan.Size = new System.Drawing.Size(232, 22);
+            this.txTenHocPhan.TabIndex = 5;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(193, 347);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(76, 17);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Giang vien";
+            // 
+            // txGV
+            // 
+            this.txGV.Location = new System.Drawing.Point(336, 342);
+            this.txGV.Name = "txGV";
+            this.txGV.Size = new System.Drawing.Size(232, 22);
+            this.txGV.TabIndex = 7;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(193, 375);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(76, 17);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Phong hoc";
+            // 
+            // txPhongHoc
+            // 
+            this.txPhongHoc.Location = new System.Drawing.Point(336, 375);
+            this.txPhongHoc.Name = "txPhongHoc";
+            this.txPhongHoc.Size = new System.Drawing.Size(232, 22);
+            this.txPhongHoc.TabIndex = 9;
+            // 
+            // txTGBT
+            // 
+            this.txTGBT.Location = new System.Drawing.Point(336, 427);
+            this.txTGBT.Name = "txTGBT";
+            this.txTGBT.Size = new System.Drawing.Size(232, 22);
+            this.txTGBT.TabIndex = 10;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(193, 427);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(119, 17);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "Thoi gian bat dau";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(193, 480);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(131, 17);
+            this.label7.TabIndex = 12;
+            this.label7.Text = "Thoi Gian Ket Thuc";
+            // 
+            // txTGKT
+            // 
+            this.txTGKT.Location = new System.Drawing.Point(336, 475);
+            this.txTGKT.Name = "txTGKT";
+            this.txTGKT.Size = new System.Drawing.Size(232, 22);
+            this.txTGKT.TabIndex = 13;
+            // 
+            // Phong
+            // 
+            this.Phong.AutoSize = true;
+            this.Phong.Location = new System.Drawing.Point(193, 526);
+            this.Phong.Name = "Phong";
+            this.Phong.Size = new System.Drawing.Size(33, 17);
+            this.Phong.TabIndex = 14;
+            this.Phong.Text = "Thu";
+            // 
+            // txthu
+            // 
+            this.txthu.Location = new System.Drawing.Point(336, 526);
+            this.txthu.Name = "txthu";
+            this.txthu.Size = new System.Drawing.Size(232, 22);
+            this.txthu.TabIndex = 15;
+            // 
+            // btnPCGD
+            // 
+            this.btnPCGD.Location = new System.Drawing.Point(274, 606);
+            this.btnPCGD.Name = "btnPCGD";
+            this.btnPCGD.Size = new System.Drawing.Size(262, 61);
+            this.btnPCGD.TabIndex = 16;
+            this.btnPCGD.Text = "Phan cong giang day";
+            this.btnPCGD.UseVisualStyleBackColor = true;
+            // 
+            // dataGridView3
+            // 
+            this.dataGridView3.BackgroundColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.dataGridView3.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView3.Location = new System.Drawing.Point(637, 342);
+            this.dataGridView3.Name = "dataGridView3";
+            this.dataGridView3.RowHeadersWidth = 51;
+            this.dataGridView3.RowTemplate.Height = 24;
+            this.dataGridView3.Size = new System.Drawing.Size(372, 255);
+            this.dataGridView3.TabIndex = 17;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(634, 294);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(171, 17);
+            this.label8.TabIndex = 18;
+            this.label8.Text = "Lich phan cong giang day";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 19;
+            this.button1.Text = "Quay ve";
+            this.button1.UseVisualStyleBackColor = false;
+            // 
+            // PhanCongGiangDay
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.ClientSize = new System.Drawing.Size(1176, 749);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.dataGridView3);
+            this.Controls.Add(this.btnPCGD);
+            this.Controls.Add(this.txthu);
+            this.Controls.Add(this.Phong);
+            this.Controls.Add(this.txTGKT);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.txTGBT);
+            this.Controls.Add(this.txPhongHoc);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.txGV);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txTenHocPhan);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.dataGridView2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "PhanCongGiangDay";
+            this.Text = "PhanCongGiangDay";
+            this.Load += new System.EventHandler(this.PhanCongGiangDay_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView3)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridView dataGridView2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txTenHocPhan;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txGV;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txPhongHoc;
+        private System.Windows.Forms.TextBox txTGBT;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox txTGKT;
+        private System.Windows.Forms.Label Phong;
+        private System.Windows.Forms.TextBox txthu;
+        private System.Windows.Forms.Button btnPCGD;
+        private System.Windows.Forms.DataGridView dataGridView3;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Button button1;
+    }
+}
---
+++ GUI_PTTK/XuatHoaDonCongTy.Designer.cs
@@ -0,0 +1,113 @@
+﻿
+namespace GUI_PTTK
+{
+    partial class XuatHoaDonCongTy
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txCongTy = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(275, 103);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(83, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Ten cong ty";
+            // 
+            // txCongTy
+            // 
+            this.txCongTy.Location = new System.Drawing.Point(410, 103);
+            this.txCongTy.Name = "txCongTy";
+            this.txCongTy.Size = new System.Drawing.Size(100, 22);
+            this.txCongTy.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(275, 163);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(62, 17);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Hoa don";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.BackgroundColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(278, 210);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(240, 150);
+            this.dataGridView1.TabIndex = 3;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.button1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.button1.Location = new System.Drawing.Point(12, 23);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Quay ve";
+            this.button1.UseVisualStyleBackColor = false;
+            // 
+            // XuatHoaDonCongTy
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txCongTy);
+            this.Controls.Add(this.label1);
+            this.Name = "XuatHoaDonCongTy";
+            this.Text = "XuatHoaDonCongTy";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txCongTy;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button1;
+    }
+}
