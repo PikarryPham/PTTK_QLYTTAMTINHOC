8d91174f4dd61cbdfca28b167cf9ef63a276f462
Phạm Ngọc Thùy Trang <43264944+PikarryPham@users.noreply.github.com>
2021-04-12 11:15:17 +0700

    add user control popup

--- GUI_PTTK/Form1.Designer.cs
+++ GUI_PTTK/Form1.Designer.cs
@@ -30,23 +30,69 @@ namespace GUI_PTTK
         private void InitializeComponent()
         {
             this.username = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.taomoikithiccqt = new System.Windows.Forms.Button();
+            this.capnhatkythiCCQT = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // username
             // 
             this.username.AutoSize = true;
-            this.username.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.username.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.username.Location = new System.Drawing.Point(668, 19);
             this.username.Name = "username";
-            this.username.Size = new System.Drawing.Size(85, 29);
+            this.username.Size = new System.Drawing.Size(70, 25);
             this.username.TabIndex = 0;
             this.username.Text = "label1";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.label1.Location = new System.Drawing.Point(178, 83);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(439, 36);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Quan ly ky thi chung chi quoc te";
+            // 
+            // taomoikithiccqt
+            // 
+            this.taomoikithiccqt.Location = new System.Drawing.Point(120, 167);
+            this.taomoikithiccqt.Name = "taomoikithiccqt";
+            this.taomoikithiccqt.Size = new System.Drawing.Size(243, 70);
+            this.taomoikithiccqt.TabIndex = 2;
+            this.taomoikithiccqt.Text = "Tao moi ky thi CCQT";
+            this.taomoikithiccqt.UseVisualStyleBackColor = true;
+            // 
+            // capnhatkythiCCQT
+            // 
+            this.capnhatkythiCCQT.Location = new System.Drawing.Point(419, 167);
+            this.capnhatkythiCCQT.Name = "capnhatkythiCCQT";
+            this.capnhatkythiCCQT.Size = new System.Drawing.Size(243, 70);
+            this.capnhatkythiCCQT.TabIndex = 3;
+            this.capnhatkythiCCQT.Text = "Cap nhat thong tin ky thi CCQT";
+            this.capnhatkythiCCQT.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(122, 288);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(243, 70);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Tao moi ky thi CCQT";
+            this.button1.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.capnhatkythiCCQT);
+            this.Controls.Add(this.taomoikithiccqt);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.username);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Name = "Form1";
@@ -59,6 +105,10 @@ namespace GUI_PTTK
         #endregion
 
         private System.Windows.Forms.Label username;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button taomoikithiccqt;
+        private System.Windows.Forms.Button capnhatkythiCCQT;
+        private System.Windows.Forms.Button button1;
     }
 }
 
---
+++ GUI_PTTK/Popup components/ComponentReuse.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GUI_PTTK.Popup_components
+{
+    public partial class Popup : UserControl
+    {
+        public Popup()
+        {
+            InitializeComponent();
+        }
+    }
+}
