fd97679bb85c38a357a1789d1144b0a457a53e16
Phạm Ngọc Thùy Trang <43264944+PikarryPham@users.noreply.github.com>
2021-04-12 20:52:15 +0700

    them trang ca nhan cho quan li

---
+++ GUI_PTTK/TrangCaNhan_QuanLy.Designer.cs
@@ -0,0 +1,150 @@
+﻿
+namespace GUI_PTTK
+{
+    partial class TrangCaNhan_QuanLy
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.username = new System.Windows.Forms.Label();
+            this.backcapnhat = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.trangcn_captknhanvien = new System.Windows.Forms.Button();
+            this.trangcn_taokithi = new System.Windows.Forms.Button();
+            this.trangcn_capnhatkithi = new System.Windows.Forms.Button();
+            this.trangcn_editthongtinhp = new System.Windows.Forms.Button();
+            this.trangcn_themthongtinhp = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // username
+            // 
+            this.username.AutoSize = true;
+            this.username.Font = new System.Drawing.Font("Microsoft Tai Le", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.username.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.username.Location = new System.Drawing.Point(503, 22);
+            this.username.Name = "username";
+            this.username.Size = new System.Drawing.Size(106, 26);
+            this.username.TabIndex = 41;
+            this.username.Text = "Username";
+            // 
+            // backcapnhat
+            // 
+            this.backcapnhat.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.backcapnhat.Location = new System.Drawing.Point(21, 22);
+            this.backcapnhat.Name = "backcapnhat";
+            this.backcapnhat.Size = new System.Drawing.Size(99, 36);
+            this.backcapnhat.TabIndex = 42;
+            this.backcapnhat.Text = "Dang xuat";
+            this.backcapnhat.UseVisualStyleBackColor = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.label1.Location = new System.Drawing.Point(209, 76);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(227, 38);
+            this.label1.TabIndex = 43;
+            this.label1.Text = "Trang ca nhan";
+            // 
+            // trangcn_captknhanvien
+            // 
+            this.trangcn_captknhanvien.Location = new System.Drawing.Point(205, 156);
+            this.trangcn_captknhanvien.Name = "trangcn_captknhanvien";
+            this.trangcn_captknhanvien.Size = new System.Drawing.Size(240, 57);
+            this.trangcn_captknhanvien.TabIndex = 44;
+            this.trangcn_captknhanvien.Text = "Cap tai khoan nhan vien";
+            this.trangcn_captknhanvien.UseVisualStyleBackColor = true;
+            // 
+            // trangcn_taokithi
+            // 
+            this.trangcn_taokithi.Location = new System.Drawing.Point(352, 252);
+            this.trangcn_taokithi.Name = "trangcn_taokithi";
+            this.trangcn_taokithi.Size = new System.Drawing.Size(240, 57);
+            this.trangcn_taokithi.TabIndex = 45;
+            this.trangcn_taokithi.Text = "Tao moi ki thi CCQT";
+            this.trangcn_taokithi.UseVisualStyleBackColor = true;
+            // 
+            // trangcn_capnhatkithi
+            // 
+            this.trangcn_capnhatkithi.Location = new System.Drawing.Point(61, 252);
+            this.trangcn_capnhatkithi.Name = "trangcn_capnhatkithi";
+            this.trangcn_capnhatkithi.Size = new System.Drawing.Size(240, 57);
+            this.trangcn_capnhatkithi.TabIndex = 46;
+            this.trangcn_capnhatkithi.Text = "Cap nhat ki thi CCQT";
+            this.trangcn_capnhatkithi.UseVisualStyleBackColor = true;
+            // 
+            // trangcn_editthongtinhp
+            // 
+            this.trangcn_editthongtinhp.Location = new System.Drawing.Point(61, 343);
+            this.trangcn_editthongtinhp.Name = "trangcn_editthongtinhp";
+            this.trangcn_editthongtinhp.Size = new System.Drawing.Size(240, 57);
+            this.trangcn_editthongtinhp.TabIndex = 48;
+            this.trangcn_editthongtinhp.Text = "Chinh sua thong tin hoc phan";
+            this.trangcn_editthongtinhp.UseVisualStyleBackColor = true;
+            // 
+            // trangcn_themthongtinhp
+            // 
+            this.trangcn_themthongtinhp.Location = new System.Drawing.Point(352, 343);
+            this.trangcn_themthongtinhp.Name = "trangcn_themthongtinhp";
+            this.trangcn_themthongtinhp.Size = new System.Drawing.Size(240, 57);
+            this.trangcn_themthongtinhp.TabIndex = 47;
+            this.trangcn_themthongtinhp.Text = "Them moi thong tin hoc phan";
+            this.trangcn_themthongtinhp.UseVisualStyleBackColor = true;
+            // 
+            // TrangCaNhan_QuanLy
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(671, 449);
+            this.Controls.Add(this.trangcn_editthongtinhp);
+            this.Controls.Add(this.trangcn_themthongtinhp);
+            this.Controls.Add(this.trangcn_capnhatkithi);
+            this.Controls.Add(this.trangcn_taokithi);
+            this.Controls.Add(this.trangcn_captknhanvien);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.backcapnhat);
+            this.Controls.Add(this.username);
+            this.Name = "TrangCaNhan_QuanLy";
+            this.Text = "TrangCaNhan_QuanLy";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label username;
+        private System.Windows.Forms.Button backcapnhat;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button trangcn_captknhanvien;
+        private System.Windows.Forms.Button trangcn_taokithi;
+        private System.Windows.Forms.Button trangcn_capnhatkithi;
+        private System.Windows.Forms.Button trangcn_editthongtinhp;
+        private System.Windows.Forms.Button trangcn_themthongtinhp;
+    }
+}
