b85c646087a43dcf8ddb68b6f98913e1592a9805
Phạm Ngọc Thùy Trang <43264944+PikarryPham@users.noreply.github.com>
2021-04-29 08:11:03 +0700

    add sql script for creating table

---
+++ PTTK_DOAN6/DAL_PTTK/DataConnection.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace DAL_PTTK
+{
+    public class DataConnection
+    {
+        public static SqlConnection GetSqlConnection()
+        {
+            string connectionString = "Server=.;Database=xxxxx;Integrated Security=true;";
+            SqlConnection con = new SqlConnection(connectionString);
+            return con;
+        }
+    }
+}
